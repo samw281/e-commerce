17acb5764075334b2c24a96057e89232c3708d18
Sam Williams <samwilliams281@gmail.com>
2022-04-17 19:15:11 -0400

    starter code + add models

---
+++ models/ProductTag.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const Product = require('./Product');
+const Tag = require('./Tag');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Product
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Tag
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
